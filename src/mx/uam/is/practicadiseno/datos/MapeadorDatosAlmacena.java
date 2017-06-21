0806393bd3c49b13b7de17e90c5757fe92037a3e
iruga090 <iruga090@github.com>
2017-06-21 01:27:01 -0500

    Error al agregar

--- src/mx/uam/is/practicadiseno/datos/MapeadorDatosAlmacena.java
+++ src/mx/uam/is/practicadiseno/datos/MapeadorDatosAlmacena.java
@@ -2,6 +2,8 @@ package mx.uam.is.practicadiseno.datos;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,27 @@ public class MapeadorDatosAlmacena implements MapeadorDatos {
 		// TODO Auto-generated method stub
 		if(!dato.equals("") && !datos.contains(dato)) {
 			datos.add(dato);
+			FileWriter fichero = null;
+	        PrintWriter pw = null;
+	        try
+	        {
+	            fichero = new FileWriter("prueba.txt",true);
+	            pw = new PrintWriter(fichero);
+
+	                pw.println(dato);
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        } finally {
+	           try {
+	           // Nuevamente aprovechamos el finally para 
+	           // asegurarnos que se cierra el fichero.
+	           if (null != fichero)
+	              fichero.close();
+	           } catch (Exception e2) {
+	              e2.printStackTrace();
+	           }
+	        }
 			return true;
 		}
 		return false;
@@ -38,8 +61,8 @@ public class MapeadorDatosAlmacena implements MapeadorDatos {
 		try {
 	         // Apertura del fichero y creacion de BufferedReader para poder
 	         // hacer una lectura comoda (disponer del metodo readLine()).
-			File archivo = new File("datos.txt");
-	         fr = new FileReader ("mx/uam/is/practicadiseno/datos/datos.txt");
+			File archivo = new File("prueba.txt");
+	         fr = new FileReader ("prueba.txt");
 	         br = new BufferedReader(fr);
 
 	         // Lectura del fichero
